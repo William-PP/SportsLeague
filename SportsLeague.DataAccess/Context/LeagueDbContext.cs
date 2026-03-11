7f9e3d49c723a4ff9e5fab8b7c7ac9fbfd2cd116
William-PP <william.perez.pareja@gmail.com>
2026-03-10 22:06:57 -0500

    correccion de errores en codigo y cambio de GenericRepository del DataAccess a el Domain

--- SportsLeague.DataAccess/Context/LeagueDbContext.cs
+++ SportsLeague.DataAccess/Context/LeagueDbContext.cs
@@ -3,7 +3,7 @@ using SportsLeague.Domain.Entities;
 
 namespace SportsLeague.DataAccess.Context;
 
-public class LeagueDbContext : Dbcontext /*clase principal de entity framework core
+public class LeagueDbContext : DbContext /*clase principal de entity framework core
                                           * conecta la app con la base de datos*/
 
 {
@@ -13,7 +13,7 @@ public class LeagueDbContext : Dbcontext /*clase principal de entity framework c
     {
     }
 
-    public Dbset<Team> teams => Set<Team>();
+    public DbSet<Team> teams => Set<Team>();
     //Esto representa la tabla teams es Tabla:teams tipo Team
     protected override void OnModelCreating(ModelBuilder modelBuilder)
         //Metodo para configurar la base de datos: se define aca claves, indices, tamaños, restricciones
@@ -21,11 +21,11 @@ public class LeagueDbContext : Dbcontext /*clase principal de entity framework c
         base.OnModelCreating(modelBuilder); //llama a la implementacion base del framework 
         modelBuilder.Entity<Team>(entity =>// aca se configurar la entidad team
         {
-            entity.Haskey(t => t.Id);//clave primaria
+            entity.HasKey(t => t.Id);//clave primaria
             entity.Property(t => t.Name)//configura name
-            .IsRquired() // not null (no puede ser null)
+            .IsRequired() // not null (no puede ser null)
             .HasMaxLength(100); // varchar(100)
-            entity.Property(t => t.city) // configura city
+            entity.Property(t => t.City) // configura city
             .IsRequired()//not null
             .HasMaxLength(100);//varchar (150)
             entity.Property(t => t.Stadium)// configura estadio
@@ -34,10 +34,10 @@ public class LeagueDbContext : Dbcontext /*clase principal de entity framework c
             .HasMaxLength(500);//varchar 500
             entity.Property(t => t.CreatedAt)// configura createdat
             .IsRequired();//not null
-            entity.Property(t => t.UpdatedAt) //configura updatedat
+            entity.Property(t => t.UpdateAt) //configura updatedat
             .IsRequired(false); // permite el null concuerda con datetime?
             entity.HasIndex(t =>t.Name) //indice unico para name
             .IsUnique(); // no permite mas con este nombre en la base de datos
-        })
+        });
     }
 }
