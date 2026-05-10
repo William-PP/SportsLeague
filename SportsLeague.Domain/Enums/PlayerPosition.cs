a42752ceb954218b705ed0ee336514c099c1b364
William-PP <william.perez.pareja@gmail.com>
2026-05-10 17:43:41 -0500

    feat(data): add seed data for SportsLeague

--- SportsLeague.Domain/Enums/PlayerPosition.cs
+++ SportsLeague.Domain/Enums/PlayerPosition.cs
@@ -2,9 +2,9 @@ namespace SportsLeague.Domain.Enums;
 
 public enum PlayerPosition
 {
-    Goalkeerper =0,
+    Goalkeeper =0,
     Defender = 1,
-    Midfilder = 2,
+    Midfielder = 2,
     Forward = 3
     //es buena practica asignar valores , asi si alguien reordena los elements del enum, los valores de la base de datos no se corrompen
 }
